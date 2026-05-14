4f30391c52fe88080b85754bd7439364de1b2505
Jonah Nestrick <undecidedapollo@gmail.com>
2026-05-14 15:01:40 -0400

    pixel

--- src/main.rs
+++ src/main.rs
@@ -10,5 +10,12 @@ fn panic_handler(_: &core::panic::PanicInfo) -> ! {
 
 #[unsafe(no_mangle)]
 extern "C" fn main() -> ! {
+    DISPCNT.write(
+        DisplayControl::new()
+            .with_video_mode(VideoMode::_3)
+            .with_show_bg2(true),
+    );
+
+    VIDEO3_VRAM.get(10, 20).unwrap().write(Color::BLUE);
     loop {}
 }
